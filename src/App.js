577efb360e2d38d724ec6b6ed46f8b139dc919ef
jjlevy <jjlevy@crimson.ua.edu>
2022-05-31 15:35:31 -0500

    working bingo cell

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+function Cell({ text, idx }) {
+    const [clicked, setClicked] = useState(false);
+    return (
+        <div
+            className="cell"
+            key={idx}
+            style={{ backgroundColor: `${clicked ? "blue" : "white"}` }}
+            onClick={() => setClicked(!clicked)}
+        >
+            {text}
+        </div>
+    );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [textBox, setTextBox] = useState("");
+
+    function shuffleArray(array) {
+        let i = array.length - 1;
+        for (i; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            const temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+        return array;
+    }
+
+    return (
+        <div>
+            <input
+                type="text"
+                value={textBox}
+                onChange={(e) => setTextBox(e.target.value)}
+                style={{ height: "100px" }}
+            />
+            <div>
+                {shuffleArray(textBox.split(" ")).map((text, idx) => (
+                    <Cell text={text} idx={idx} />
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default App;
