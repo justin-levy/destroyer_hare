c4a2611d86ee602cfd74a6bd36cddde617ce4bc8
jjlevy <jjlevy@crimson.ua.edu>
2022-07-01 03:13:31 -0500

    removed image paths?

--- src/killerBunnies/Card.jsx
+++ src/killerBunnies/Card.jsx
@@ -34,9 +34,7 @@ export function PlayingCard({ card, idx, basicFunctions, deck }) {
                 menuButton={
                     <Card>
                         <Card.Body className="p-1">
-                            <Card.Img
-                                src={`destroyer_hare/blue/${cardImg}.png`}
-                            ></Card.Img>
+                            <Card.Img src={`blue/${cardImg}.png`}></Card.Img>
                         </Card.Body>
                     </Card>
                 }
--- src/killerBunnies/Player.jsx
+++ src/killerBunnies/Player.jsx
@@ -170,7 +170,7 @@ function Player({
                     title={`Deck : ${getLength(gameState.deck)} Cards`}
                     handleClick={() => draw()}
                     actionTitle="Draw"
-                    picture="destroyer_hare/blue.png"
+                    picture="blue.png"
                 />
 
                 <Deck
@@ -178,7 +178,7 @@ function Player({
                     title={`Carrots : ${getLength(gameState.carrotDeck)} Cards`}
                     handleClick={() => drawCarrot()}
                     actionTitle="Draw"
-                    picture="destroyer_hare/carrot.png"
+                    picture="carrot.png"
                 />
 
                 <Deck
@@ -186,7 +186,7 @@ function Player({
                     title={`Market`}
                     handleClick={() => console.log()}
                     actionTitle=""
-                    picture={`destroyer_hare/${gameState.market.deck}/${gameState.market.id}.png`}
+                    picture={`${gameState.market.deck}/${gameState.market.id}.png`}
                 />
 
                 <Deck
@@ -201,7 +201,7 @@ function Player({
                     picture={
                         gameState.discardedDeck &&
                         getLength(gameState.discardedDeck)
-                            ? `destroyer_hare/blue/${
+                            ? `blue/${
                                   Object.entries(gameState.discardedDeck)[
                                       getLength(gameState.discardedDeck) - 1
                                   ][1].id
@@ -231,7 +231,7 @@ function Player({
                             title={`Run ${idx + 1}`}
                             handleClick={() => playCard()}
                             actionTitle="Move in Run"
-                            picture="destroyer_hare/carrot.png"
+                            picture="carrot.png"
                             key={idx}
                         />
                     ))}
