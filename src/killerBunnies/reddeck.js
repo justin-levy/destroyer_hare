fec93ce97111fb35c260166fcd4e3364e7845cdd
jjlevy <jjlevy@crimson.ua.edu>
2022-07-05 00:30:40 -0500

    starting js files for both red decks

---
+++ src/killerBunnies/reddeck.js
@@ -0,0 +1,66 @@
+// 0166	Congenial Bunny - Red
+// 0167	Gleeful Bunny - Red
+// 0168	Lumbering Bunny - Red
+// 0169	Sinister Bunny - Red
+// 0170	Timid Bunny - Red
+// 0191	Holographic Bunny
+
+// 0171	Choose a Carrot
+// 0172	Choose a Carrot
+// 0173	Choose 2 Carrots
+
+// 0174	Feed the Bunny 3/3
+// 0175	Feed All Your Bunnies 1/1
+// 0176	Everyone Feed a Bunny 1/1
+
+// 0177	Suction Cup Weapon Level 01
+// 0178	Mousetrap Weapon Level 03
+// 0179	Grappling Hook Weapon Level 05
+// 0180	Centrifuge Weapon Level 07
+// 0181	Cruise Missile Prime Weapon Level 09
+// 0182	Quite Irascible Diffractable Cheese Balls Weapon Level 11
+
+// 0185	Barrier
+// 0186	Barrier
+
+// 0183	Activate The Minilith
+// 0184	Bankruptcy
+// 0187	Carrot Exchange
+// 0188	Clumsy Congenial
+// 0189	Dude, Where's My Carrot?
+// 0190	Europan Robotic Titanium Termites
+// 0192	Quindruple Lucky Clover
+// 0193	The Mystery Urn
+// 0194	Rainbo
+// 0195	Red Light District
+// 0196	Rooney's Weapons Emporium Closed
+
+// 0197	Bounty Mounty
+// 0198	Free Cabbage And Water
+// 0199	Free Defense
+// 0200	H. E. M. P.
+// 0201	Half Price Coupon - Rooney’s Weapons Emporium
+// 0202	Show Me the Bunny
+
+// 0203	Bunny to the Future
+// 0204	Guardian Angle
+
+// 0205	Terrible Misfortune – Gas Main
+// 0206	Terrible Misfortune – Quicksand
+// 0207	Zep Tepi
+
+// 0208	1 Dolla
+// 0209	2 Dolla
+// 0210	3 Dolla
+// 0211	3 Dolla
+// 0212	3 Dolla
+// 0213	5 Dolla
+// 0214	5 Dolla
+// 0215	10 Dolla
+
+// 0216	Rooney's Weapons Emporium Starter 3/Level
+
+// 0217	Teal'c Carrot #13
+// 0218	Russell Carrot #14
+// 0219	Buck Carrot #15
+// 0220	Gus Carrot #16
